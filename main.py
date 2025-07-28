ac79a8863fa65244b4f4c06e251403da969faa13
Jeevitha D S <jeevithads16@gmail.com>
2025-07-28 11:53:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 import mlflow
 from dagshub import init
 from data_loader import load_data
-from data_preprocessing import preprocess_data
+from data_preprocessing import preprocess_data 
 from model_training import train_models
 from model_evaluation import evaluate_models
 
